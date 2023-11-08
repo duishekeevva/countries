a4300225572b83ae6a7adc941374a363ee2f2590
Elba <elba.duishekeeva16@gmail.com>
2023-11-09 00:20:18 +0600

    weather

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ const search = document.querySelector('#search')
 const searchBox = document.querySelector('.search-wrapper')
 const submit = document.querySelector('#submit')
 const searchInput = document.querySelector('#searchInput')
-const name=document.querySelector('#name')
-const image=document.querySelector('#image')
-const language=document.querySelector('#languages')
-const currency=document.querySelector('#currency')
-const capital=document.querySelector('#capital')
-const map=document.querySelector('#map')
-const result=document.querySelector('.result')
+const name = document.querySelector('#name')
+const image = document.querySelector('#image-search')
+const language = document.querySelector('#languages')
+const currency = document.querySelector('#currency')
+const capital = document.querySelector('#capital')
+const map = document.querySelector('#map')
+const country = document.querySelector('#country')
+const city = document.querySelector('#city')
+const date = document.querySelector('#date')
+const temp = document.querySelector('#temp')
+const tempText = document.querySelector('#temp-text')
+const result = document.querySelector('.result')
 
 
 const handleGetCountries = () => {
@@ -48,7 +53,7 @@ search.addEventListener('change', () => {
     }
 })
 
-submit.addEventListener('click', () => {
+const handleSearch = () => {
     let value = searchInput.value;
     result.classList.remove('hidden')
     fetch(`https://restcountries.com/v3.1/name/${value}`)
@@ -56,12 +61,33 @@ submit.addEventListener('click', () => {
         .then(json => {
             console.log(json)
             name.innerHTML = json[0].name.common
-            image.src=json[0].flags.png
-            language.innerHTML='Язык: '+Object.values(json[0].languages)
-            currency.innerHTML=Object.values(json[0].currencies).map(el=>el.symbol)
-            capital.innerHTML=json[0].capital
-            map.href=json[0].maps.googleMaps
+            image.src = json[0].flags.png
+            language.innerHTML = 'Язык: ' + Object.values(json[0].languages)
+            currency.innerHTML = 'Валюта: '+Object.values(json[0].currencies).map(el => el.symbol)
+            capital.innerHTML = 'Столица: '+json[0].capital
+            map.href = json[0].maps.googleMaps
+            fetch(`http://api.weatherapi.com/v1/current.json?key=6a07b5bcc8604fbca4260852230811&lang=ru&q=${json[0].capital}`)
+                .then(res => res.json())
+                .then(json => {
+                    searchInput.value = ''
+                    console.log(json)
+                    city.innerHTML = 'Город: ' + json.location.region
+                    date.innerHTML ='Дата: ' +  json.location.localtime
+                    temp.innerHTML = 'Температура: ' + json.current.temp_c
+                    tempText.innerHTML = json.current.condition.text
+                })
+
         })
 
+}
+
+submit.addEventListener('click', () => handleSearch())
+
+searchInput.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSearch()
+    }
 })
+
+
 
